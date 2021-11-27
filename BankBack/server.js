8f9fad538e751fa5633b7573671e78bbb8407b26
Walter Orozco <walterof1@hotmail.com>
2021-11-27 16:20:47 -0600

    add variables de entorno

---
+++ BankBack/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+var cors = require('cors');
+
+// MYSQL
+const mysql = require('mysql')
+const db = mysql.createConnection({
+host: process.env.DATABASE_URL , // "",
+user: process.env.DATABASE_USER , // "",
+password: process.env.DATABASE_PASS , // "",
+database: process.env.DATABASE_DB  // "" 
+})
+
+module.exports = db;
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+const bodyParser = require('body-parser');
+const routes = require('./rutas/routes');
+
+db.on('open', () => {
+    console.log('connected...') 
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+
+routes(app);
+
+app.listen('3000', () =>{
+    console.log('Listening on port 3000');
+})
